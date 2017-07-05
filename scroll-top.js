fecca9928d4dc18706eccf646b7c298458e335f2
jkofsky <jkofsky@cox.net>
2017-07-05 13:15:42 -0500

    Add files via upload
    Modified to include passing variables and Added comments

---
+++ scroll-top.js
@@ -0,0 +1,26 @@
+/**
+ * @link      https://github.com/bluezed/yii2-scroll-top
+ * @copyright Copyright (c) 2015 Thomas Geppert
+ * @license   https://github.com/bluezed/yii2-scroll-top/blob/master/LICENSE
+ */
+
+$(function () {
+
+    var btnScroller = $('#btn-top-scroller');
+    var scrollerTriggerPoint = $('html, body').offset().top + 150;
+
+    $(document).on('scroll', function () {
+        var pos = $(window).scrollTop();
+        if (pos > scrollerTriggerPoint && !btnScroller.is(':visible')) {
+            btnScroller.fadeIn();
+        } else if (pos < scrollerTriggerPoint && btnScroller.is(':visible')) {
+            btnScroller.fadeOut();
+        }
+    }).scroll();
+
+    btnScroller.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+});
